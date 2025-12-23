0076584ef43a055f29835e0e0f20297ed5a0abb8
Nexxxit <varsik.arsik@mail.ru>
2025-12-23 17:20:08 +0500

    chore: add CheckIcon PlusIcon icons

---
+++ src/shared/ui/icons/PlusIcon.tsx
@@ -0,0 +1,7 @@
+import { type SVGProps } from "react"
+
+export function Plus(props: SVGProps<SVGSVGElement>) {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" {...props}><title>Plus SVG Icon</title><path fill="none" stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="M12 20v-8m0 0V4m0 8h8m-8 0H4"></path></svg>
+    )
+  }
