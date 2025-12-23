51ef0248abe46d94c3b938294ee9998fe4eb4d2f
Nexxxit <varsik.arsik@mail.ru>
2025-12-23 15:36:03 +0500

    chore: add services mock data

---
+++ src/features/services-catalog/model/services.mock.ts
@@ -0,0 +1,7 @@
+import type { Service } from "./types.ts";
+
+export const ServicesMock: Service[] = [
+  { id: "11", title: "Выбор места", price: 1500 },
+  { id: "12", title: "Доп. багаж 10кг", price: 3600 },
+  { id: "13", title: "Приоритетная посадка", price: 950 },
+];
