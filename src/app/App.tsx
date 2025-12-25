8ef58426d152f07098ce26bc1be45d65317e0c8e
Nexxxit <varsik.arsik@mail.ru>
2025-12-25 20:44:30 +0500

    feat: add ServicePage to App and update App style

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,11 @@
+import ServicePage from "../pages/services/ServicePage.tsx";
+
 function App() {
   return (
-    <>
-    </>
-  )
+    <div className={"bg-zinc-50 min-h-screen"}>
+      <ServicePage />
+    </div>
+  );
 }
 
-export default App
+export default App;
